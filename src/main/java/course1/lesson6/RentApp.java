a0c7bc9cda81e567ee218eec2980856130f5245b
Evgeniy Gusev <emgusev@yandex-team.ru>
2022-01-19 21:16:09 +0300

    Lesson 6

---
+++ src/main/java/course1/lesson6/RentApp.java
@@ -0,0 +1,31 @@
+package course1.lesson6;
+
+public class RentApp {
+
+    public static void main(String[] args) {
+        Car car = new Car("toyota", 2012);
+        System.out.println();
+
+
+        Van van = new Van("Ford", 2010, 7);
+        Car cabrio = new Cabrio("Peugeot", 2015, true);
+
+        Car[] cars = {car, cabrio, van};
+
+        for (Car c : cars) {
+            System.out.println(c.getModel());
+        }
+
+
+        System.out.println();
+        System.out.println("Only vans");
+        for (Car c : cars) {
+            if (c instanceof Van) {
+                Van v = (Van) c;
+                System.out.println("This is a van " + v.getSeats());
+            }
+        }
+
+        //System.out.println(van);
+    }
+}
